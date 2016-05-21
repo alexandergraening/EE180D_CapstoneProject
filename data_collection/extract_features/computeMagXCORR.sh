0e542f409a6be3c87d66420ece3e434a5e11b19b
root <root@beaglebone.localdomain>
2016-05-21 22:38:07 +0000

    fixed the XCORR routines

--- data_collection/extract_features/computeMagXCORR.sh
+++ data_collection/extract_features/computeMagXCORR.sh
@@ -6,6 +6,8 @@ for i in $(seq 1 60);
 do
 	./magnitudeXCORR ../raw/accMag_${i}e.csv ../raw/accMag_${i}w.csv ../features/accXCORR_${i}.csv 20
 	./magnitudeXCORR ../raw/gyrMag_${i}e.csv ../raw/gyrMag_${i}w.csv ../features/gyrXCORR_${i}.csv 20
+	./magnitudeXCORR ../raw/accMag_${i}w.csv ../raw/gyrMag_${i}w.csv ../features/wristXCORR_${i}.csv 20
+	./magnitudeXCORR ../raw/accMag_${i}e.csv ../raw/gyrMag_${i}e.csv ../features/elbowXCORR_${i}.csv 20
 done
 
 
