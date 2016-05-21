e8ffeffc6afb92efcdafaf81d4aab8a4d8905f95
root <root@beaglebone.localdomain>
2016-05-21 23:53:27 +0000

    nothing

--- data_collection/extract_features/compute_features.sh
+++ data_collection/extract_features/compute_features.sh
@@ -14,11 +14,11 @@ do
 ./findAbsExtrema ../raw/mdo_${i}e.csv ../features/accMaxMin_${i}e.csv ../features/gyrMaxMin_${i}e.csv
 
 #./compute_cross_correlation_peaks.sh ../raw/mdo_${i}e.csv
-cp waveform_peaks_output_x.csv ../raw/mdo_${i}_Px.csv
-cp waveform_peaks_output_y.csv ../raw/mdo_${i}_Py.csv
-cp xcorr_waveform_peaks_output_x.csv ../raw/mdo_${i}_xcorr_Pxy.csv
-cp xcorr_waveform_peaks_output_y.csv ../raw/mdo_${i}_xcorr_Pyx.csv
-cp xcorr_extrema.dat ../raw/mdo_${i}_xcorr_extrema.dat
+#cp waveform_peaks_output_x.csv ../raw/mdo_${i}_Px.csv
+#cp waveform_peaks_output_y.csv ../raw/mdo_${i}_Py.csv
+#cp xcorr_waveform_peaks_output_x.csv ../raw/mdo_${i}_xcorr_Pxy.csv
+#cp xcorr_waveform_peaks_output_y.csv ../raw/mdo_${i}_xcorr_Pyx.csv
+#cp xcorr_extrema.dat ../raw/mdo_${i}_xcorr_extrema.dat
 #./bubble_sort_norm ../raw/mdo_${i}_xcorr_Pxy.csv ../raw/mdo_${i}_xcorr_Pyx.csv ../raw/mdo_${i}_xcorr_Pxy_sortnorm.csv ../raw/mdo_${i}_xcorr_Pyx_sortnorm.csv
 #./bubble_sort_norm ../raw/mdo_${i}_Px.csv ../raw/mdo_${i}_Py.csv ../raw/mdo_${i}_Px_sortnorm.csv ../raw/mdo_${i}_Py_sortnorm.csv
 done
