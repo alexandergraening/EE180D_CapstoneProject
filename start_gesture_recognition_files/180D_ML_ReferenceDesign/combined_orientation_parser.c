71da716dc1198a959b97e034eeaadc5ef1274f89
root <root@beaglebone.localdomain>
2016-05-21 23:12:25 +0000

    Added a significant amount of code to enable start gesture recognition

---
+++ start_gesture_recognition_files/180D_ML_ReferenceDesign/combined_orientation_parser.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+
+int main()
+{
+	FILE* inputFile1 = fopen("orientation1_parsed.txt", "r");
+	FILE* outputFile = fopen("combined_orientations_parsed.txt","w");
+	if (inputFile1 == NULL || outputFile == NULL)
+	{
+		printf("Error opening file!\nExit\n");
+		return 1;
+	}
+	printf("Opened files successfully!\n");
+
+//	fpos_t inputFilePos;
+	//getfilepos(inputFile,&inputFilePos);
+
+	int outputValues1;
+	int outputValues2;
+	int outputValues3;
+	printf("Successfully created storage ints!\n");
+	
+	printf("About to read from files.\n");
+	fprintf(outputFile,"250 3 5\n");
+	int k;
+	fscanf(inputFile1,"%d %d %d",&outputValues1,&outputValues2,&outputValues3);
+	for (k = 0; k < 100;++k)
+	{
+		fscanf(inputFile1,"%d %d %d",&outputValues1,&outputValues2,&outputValues3);
+		fprintf(outputFile,"%d %d %d\n",outputValues1,outputValues2, outputValues3);
+	}
+	fclose(inputFile1);
+
+	FILE* inputFile2 = fopen("orientation2_parsed.txt", "r");
+
+	if (inputFile2 == NULL)
+	{
+		printf("Error opening file!\nExit\n");
+		return 1;
+	}
+	printf("Opened files successfully!\n");
+	fscanf(inputFile2,"%d %d %d",&outputValues1,&outputValues2,&outputValues3);
+	for (k = 0; k < 100;++k)
+	{
+		fscanf(inputFile2,"%d %d %d",&outputValues1,&outputValues2,&outputValues3);
+		fprintf(outputFile,"%d %d %d\n",outputValues1,outputValues2, outputValues3);
+	}
+	fclose(inputFile2);
+
+	FILE* inputFile3 = fopen("orientation3_parsed.txt", "r");
+
+	if (inputFile3 == NULL)
+	{
+		printf("Error opening file!\nExit\n");
+		return 1;
+	}
+	printf("Opened files successfully!\n");
+	fscanf(inputFile3,"%d %d %d",&outputValues1,&outputValues2,&outputValues3);
+	for (k = 0; k < 100;++k)
+	{
+		fscanf(inputFile3,"%d %d %d",&outputValues1,&outputValues2,&outputValues3);
+		fprintf(outputFile,"%d %d %d\n",outputValues1,outputValues2, outputValues3);
+	}
+	fclose(inputFile3);
+
+	FILE* inputFile4 = fopen("orientation4_parsed.txt", "r");
+
+	if (inputFile4 == NULL)
+	{
+		printf("Error opening file!\nExit\n");
+		return 1;
+	}
+	printf("Opened files successfully!\n");
+	fscanf(inputFile4,"%d %d %d",&outputValues1,&outputValues2,&outputValues3);
+	for (k = 0; k < 100;++k)
+	{
+		fscanf(inputFile3,"%d %d %d",&outputValues1,&outputValues2,&outputValues3);
+		fprintf(outputFile,"%d %d %d\n",outputValues1,outputValues2, outputValues3);
+	}
+	fclose(inputFile4);
+	
+	FILE* inputFile5 = fopen("orientation5_parsed.txt", "r");
+
+	if (inputFile5 == NULL)
+	{
+		printf("Error opening file!\nExit\n");
+		return 1;
+	}
+	printf("Opened files successfully!\n");
+	fscanf(inputFile5,"%d %d %d",&outputValues1,&outputValues2,&outputValues3);
+	for (k = 0; k < 100;++k)
+	{
+		fscanf(inputFile5,"%d %d %d",&outputValues1,&outputValues2,&outputValues3);
+		fprintf(outputFile,"%d %d %d\n",outputValues1,outputValues2, outputValues3);
+	}
+	fclose(inputFile5);
+
+	fclose(outputFile);
+	printf("Files closed!\n");
+
+	return 0;
+}
--- start_gesture_recognition_files/README.md
+++ start_gesture_recognition_files/README.md
@@ -1,1 +1,3 @@
-#This is a README...
+This directory contains the files necessary to achieve our goal of user feedback.
+
+We used the 180D machine learning orientation reference design to achieve this. There are likely more efficient ways, but this way works well.
