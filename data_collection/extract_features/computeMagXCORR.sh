7c034c1ae70c902b5350d74612ae965920859bce
root <root@beaglebone.localdomain>
2016-05-21 23:28:09 +0000

    added capability to compute mass cross correlations of magnitude csv's

---
+++ data_collection/extract_features/computeMagXCORR.sh
@@ -0,0 +1,11 @@
+echo ""
+echo "Beginning XCORR computation of Elbow Acc with Wrist Acc"
+echo ""
+
+for i in $(seq 1 60);
+do
+	./magnitudeXCORR ../raw/accMag_${i}e.csv ../raw/accMag_${i}w.csv ../features/accXCORR_${i}.csv 20
+	./magnitudeXCORR ../raw/gyrMag_${i}e.csv ../raw/gyrMag_${i}w.csv ../features/gyrXCORR_${i}.csv 20
+done
+
+
