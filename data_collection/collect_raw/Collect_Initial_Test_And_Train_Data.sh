2b21afa1dca3e47c8db70cb02de046748e3f3cc8
root <root@beaglebone.localdomain>
2016-05-22 01:13:53 +0000

    Just a few modifications to update the Makefile in data_collection/collect_raw and enabled begin and end sample numbers for Collect_Initial_Test_And_Train_Data.sh

--- data_collection/collect_raw/Collect_Initial_Test_And_Train_Data.sh
+++ data_collection/collect_raw/Collect_Initial_Test_And_Train_Data.sh
@@ -1,8 +1,18 @@
+#!/bin/sh
+
+#
+# Usage: Enter starting sample number and ending sample number (inclusive)
+#
+
+if [ "$#" -ne 2 ]; then
+		echo "Enter starting sample number and ending sample number"
+		exit
+	fi
 
 echo "Get ready to practice your freethrows!"
 echo ""
 
-for i in $(seq 1 10);
+for i in $(seq $1 $2);
 do
 echo "About to Acquire dataset $i, press Enter to start"
 ./wait_for_verification
@@ -15,29 +25,29 @@ echo "Done"
 echo ""
 done
 
-echo "Please prepare to break the freethrow into multiple parts"
-for i in $(seq 1 5);
-do
-echo "Press enter, then raise the ball above your head."
-./wait_for_verification
-./Dual_SensorTile_Acquire.sh -t 3
-sleep 8
-cp motion_data_output_1.csv ../raw/firstPartial_${i}w.csv
-cp motion_data_output_2.csv ../raw/firstPartial_${i}e.csv
-echo "Done"
-echo ""
-done
+#echo "Please prepare to break the freethrow into multiple parts"
+#for i in $(seq 1 5);
+#do
+#echo "Press enter, then raise the ball above your head."
+#./wait_for_verification
+#./Dual_SensorTile_Acquire.sh -t 3
+#sleep 8
+#cp motion_data_output_1.csv ../raw/firstPartial_${i}w.csv
+#cp motion_data_output_2.csv ../raw/firstPartial_${i}e.csv
+#echo "Done"
+#echo ""
+#done
 
-for i in $(seq 1 5);
-do
-echo "Raise the ball above your head, then press enter, then shoot."
-./wait_for_verification
-./Dual_SensorTile_Acquire.sh -t 3
-sleep 8
-cp motion_data_output_1.csv ../raw/secondPartial_${i}w.csv
-cp motion_data_output_2.csv ../raw/secondPartial_${i}e.csv
-echo "Done"
-echo ""
-done
+#for i in $(seq 1 5);
+#do
+#echo "Raise the ball above your head, then press enter, then shoot."
+#./wait_for_verification
+#./Dual_SensorTile_Acquire.sh -t 3
+#sleep 8
+#cp motion_data_output_1.csv ../raw/secondPartial_${i}w.csv
+#cp motion_data_output_2.csv ../raw/secondPartial_${i}e.csv
+#echo "Done"
+#echo ""
+#done
 
 echo "Data collection completed"
